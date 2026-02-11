ef2cbbf8de56d21393bd851e0eefe48c22c9f260
VladimirG-1971 <vova16061971@gmail.com>
2026-02-11 15:48:46 +0300

    Updates

--- assets/BufferResource-DjqmTyRd.js
+++ assets/BufferResource-DjqmTyRd.js
@@ -1,4 +1,4 @@
-import{w as U,m as g,n as c,u as S,B as _,o as m,E as I,p as h}from"./index-BsuMSbK6.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,m as g,n as c,u as S,B as _,o as m,E as I,p as h}from"./index-DE0t1KsX.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
--- assets/webworkerAll-B510Sxhh.js
+++ assets/webworkerAll-B510Sxhh.js
@@ -1,4 +1,4 @@
-import{G as R,b as S,s as b,M as P,a0 as I,Y as B,aa as T,T as _,ap as O,K as A,w as C,a9 as E,L as k}from"./index-BsuMSbK6.js";import{F as z}from"./Filter-nK_4EkHG.js";var U=`in vec2 aPosition;
+import{G as R,b as S,s as b,M as P,a0 as I,Y as B,aa as T,T as _,ap as O,K as A,w as C,a9 as E,L as k}from"./index-DE0t1KsX.js";import{F as z}from"./Filter-BflQZInX.js";var U=`in vec2 aPosition;
 out vec2 vTextureCoord;
 
 uniform vec4 uInputSize;
